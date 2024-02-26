1c7bba47ab4547581fdaa14f27fb31ef64f402ae
evangelosIlias <evangelos.ilias87@gmail.com>
2024-02-26 20:32:09 +0000

    Test to show a generated ticket with status one

--- tests/Feature/Controller/TicketsControllerTest.php
+++ tests/Feature/Controller/TicketsControllerTest.php
@@ -11,13 +11,26 @@ class TicketsControllerTest extends TestCase
 {   
     use RefreshDatabase;
 
-    public function test_it_can_show_a_generated_ticket_with_status_0()
+    public function test_it_can_show_a_generated_ticket_with_status_zero()
     {   
         $this->withoutExceptionHandling();
 
         $ticket = Ticket::factory()->create(['status' => false]);
     
         $response = $this->json('get', route('ticket.index', ['status' => 'closed']));
+    
+        $this->assertFalse($ticket->fresh()->status);
+
+        $response->assertStatus(200);
+    }
+
+    public function test_it_can_show_a_generated_ticket_with_status_one()
+    {   
+        $this->withoutExceptionHandling();
+
+        $ticket = Ticket::factory()->create(['status' => false]);
+    
+        $response = $this->json('get', route('ticket.index', ['status' => 'open']));
     
         $this->assertFalse($ticket->fresh()->status);
 
