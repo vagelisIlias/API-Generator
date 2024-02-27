85a75401b447fd588c3c2b6d23aa13a9da79c7d2
evangelosIlias <evangelos.ilias87@gmail.com>
2024-02-27 12:04:17 +0000

    Refactored the TicketControllerTest and updated composer

--- tests/Feature/Controller/TicketsControllerTest.php
+++ tests/Feature/Controller/TicketsControllerTest.php
@@ -2,10 +2,11 @@
 
 namespace Tests\Feature\Controller;
 
-use App\Models\Ticket;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
+use App\Models\Ticket;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class TicketsControllerTest extends TestCase
 {
@@ -16,12 +17,17 @@ class TicketsControllerTest extends TestCase
         $this->withoutExceptionHandling();
 
         $ticket = Ticket::factory()->create(['status' => false]);
+        Ticket::factory()->create(['status' => true]);
 
         $response = $this->json('get', route('ticket.index', ['status' => 'open']));
 
-        $this->assertFalse($ticket->fresh()->status);
-
         $response->assertStatus(200);
+
+        $content = $response->getOriginalContent();
+
+        $this->assertInstanceOf(LengthAwarePaginator::class, $content);
+        $this->assertEquals(1, $content->total());
+        $this->assertEquals($ticket->id, $content->first()->id);
     }
 
     public function test_it_can_show_a_generated_ticket_with_status_one()
@@ -32,9 +38,12 @@ class TicketsControllerTest extends TestCase
 
         $response = $this->json('get', route('ticket.index', ['status' => 'closed']));
 
-        $this->assertTrue($ticket->fresh()->status);
-
         $response->assertStatus(200);
+        $content = $response->getOriginalContent();
+
+        $this->assertInstanceOf(LengthAwarePaginator::class, $content);
+        $this->assertEquals(1, $content->total());
+        $this->assertEquals($ticket->id, $content->first()->id);
     }
 
     public function test_it_can_show_list_of_tickets_with_corresponding_user_email()
@@ -47,34 +56,36 @@ class TicketsControllerTest extends TestCase
         $response = $this->json('get', route('ticket.user', ['email' => $ticket]));
 
         $response->assertStatus(200);
+        
+        $content = $response->getOriginalContent();
+
+        $this->assertInstanceOf(LengthAwarePaginator::class, $content);
+    
+        if (! $content->isEmpty()) {
+            $this->assertEquals($userEmail, $content->first()->user_email);
+        } 
     }
 
     public function test_it_can_return_the_stats()
     {
         $this->withoutExceptionHandling();
 
-        Ticket::factory()->count(5)->create(['status' => false]);
-        Ticket::factory()->count(3)->create(['status' => true]);
+        Ticket::factory()->count(5)->create(['status' => false, 'user_email' => 'most@test.test', 'user_name' => 'Most']);
+        Ticket::factory()->count(3)->create(['status' => true, 'user_email' => 'less@test.test', 'user_name' => 'Less']);
 
         $response = $this->json('get', route('stats'));
-
-        $totalTickets = Ticket::count();
-        $unprocessedTickets = Ticket::where('status', false)->count();
-
-        $userWithMostTickets = Ticket::select('user_name', DB::raw('COUNT(user_email) as total'))
-            ->groupBy('user_name')
-            ->orderBy('total', 'desc')
-            ->first();
 
         $lastTicket = Ticket::where('status', true)
             ->orderBy('updated_at', 'desc')
             ->first();
 
-        $response->assertStatus(200, [
-            'total_tickets' => $totalTickets,
-            'unprocessed_tickets' => $unprocessedTickets,
-            'most_tickets_by_user' => $userWithMostTickets->user_name.' ('.$userWithMostTickets->total.')',
+        $response->assertStatus(200);
+        
+        $this->assertEquals([
+            'total_tickets' => 8,
+            'unprocessed_tickets' => 5,
+            'most_tickets_by_user' => 'Most (5)',
             'last_processed' => $lastTicket?->updated_at,
-        ]);
+        ], $response->getOriginalContent());
     }
 }
